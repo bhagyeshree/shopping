f715e2b64ee80a58d3f1c0b908c221281d162295
shree <bhagyeshree.gaikwad@gmail.com>
2017-12-01 17:25:15 -0500

    My Project SetUp Completed Successfully

---
+++ shopping-ui/src/main/java/com/example/shoppingui/ShoppingUiApplication.java
@@ -0,0 +1,12 @@
+package com.example.shoppingui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingUiApplication.class, args);
+	}
+}
